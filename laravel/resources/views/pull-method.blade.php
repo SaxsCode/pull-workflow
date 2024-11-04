ff3692e7ad3a6a2837b8e201083fc3001bbdd325
SaxsCode <maxsax.sch@gmail.com>
2024-11-04 21:31:21 +0100

    styles: let AI generate some css lol

--- laravel/resources/views/pull-method.blade.php
+++ laravel/resources/views/pull-method.blade.php
@@ -1,14 +1,17 @@
-<div>
-    <div class="active">
+
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/styles.css') }}" />
+
+<div class="container">
+    <div class="active" data-count="3">
         <h2>Active projects</h2>
-            <li>test</li>
-            <li>test</li>
-            <li>test</li>
+        <li>Pull Method project</li>
+        <li>Colemak leren</li>
+        <li>VIM leren</li>
     </div>
     <div class="holding">
         <h2>Holding tank</h2>
-        <li>test</li>
-        <li>test</li>
+        <li>Hour tracker koppelen met PM</li>
+        <li>VIM opzetten met colemak</li>
         <li>test</li>
         <li>test</li>
         <li>test</li>
