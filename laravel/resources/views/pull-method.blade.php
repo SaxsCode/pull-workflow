5862de74bd86791c18cbfa4ee8535d85fd00d9f5
SaxsCode <maxsax.sch@gmail.com>
2024-11-04 19:48:06 +0100

    feat: setup view

---
+++ laravel/resources/views/pull-method.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <div class="active">
+        <h2>Active projects</h2>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+    </div>
+    <div class="holding">
+        <h2>Holding tank</h2>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+    </div>
+</div>
